6989cdb2ca9b17bcfbb487929b5032e993575848
Andy VanOsten <avanosten@wpi.edu>
2020-04-20 15:40:36 -0400

    Added more code for feature integration

--- src/EKF_SLAM.py
+++ src/EKF_SLAM.py
@@ -6,6 +6,13 @@ class EKF_SLAM():
     l = 1      # wheelbase length
     fwd_stddev = 1      # Standard deviation of encoder (forward distance) readings
     steer_stddev = 1    # Standard deviation of steering angle (avg angle of front wheel angles)
+    sigma_x = 1         # Variance of feature x dimension
+    sigma_y = 1         # Variance of feature y dimension
+    sigma_z = 1         # Variance of feature z dimension
+    sigma_s = 1         # Variance of feature s dimension
+
+    l_c = 1             # horizontal distance from center of rear axle to camera frame
+    h_c = 1             # vertical distance from rear axle to camera frame
     
     def __init__(self):
         #rospy.init_node('EKF_SLAM')
@@ -56,10 +63,42 @@ class EKF_SLAM():
             [ (1/self.l) * np.tan(state_old[3]), 0 ],
             [ 0, 1 ]
         ])
-        M = np.array([[self.fwd_stddev, 0], [0, self.steer_stddev]])
-        R = V.dot(M).dot(V.T)
-        cov_est = G.dot(cov_old).dot(G.T) + F_x.T.dot(R).dot(F_x)
+        M = np.diag((self.fwd_stddev, self.steer_stddev))
+        R = V.dot(M).dot(V.T)   # R = V . M . V^T
+        # Estimate new covariance matrix
+        cov_est = G.dot(cov_old).dot(G.T) + F_x.T.dot(R).dot(F_x)   # Sigma = G . cov_old . G^T + F_x^T . R . F_x
         
+        Q = np.diag((self.sigma_x, self.sigma_y, self.sigma_z, self.sigma_s))
+
+        num_features,_ = features.shape
+        for i in range(num_features):
+            T_CR = self.transform_cam_robot()
+            T_RW = self.transform_robot_world(state_est[0:3])
+            new_feature = T_CR.dot(T_RW).dot(np.hstack((features[i], 1))).flatten()
+            new_feature[3] = features[i,3]
+
+            for k in range(N + 1):
+                pass
+
+    # Homogeneous transformation matrix from the camera frame to the robot frame (center of the rear axle)
+    def transform_cam_robot(self):
+        T_CR = np.array([
+            [1, 0, 0, self.l_c],
+            [0, 1, 0, 0],
+            [0, 0, 1, self.h_c],
+            [0, 0, 0, 1]
+        ])
+        return T_CR
+
+    # Homogeneous transformation matrix from the robot to the world
+    def transform_robot_world(self, state):
+        T_RW = np.array([
+            [  np.cos(state[2]), np.sin(state[2]), 0, -state[0]*np.cos(state[2]) - state[1]*np.sin(state[2]) ],
+            [ -np.sin(state[2]), np.cos(state[2]), 0,  state[0]*np.sin(state[2]) - state[1]*np.cos(state[2]) ],
+            [ 0, 0, 1, 0 ],
+            [ 0, 0, 0, 1 ]
+        ])
+        return T_RW
 
 if __name__ == "__main__":
     slam = EKF_SLAM()
